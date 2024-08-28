ed6283a26c97a43ed9b2c65ce5753d6d84c35f4b
Ayush Tripathi <ayushtripathi1025@gmail.com>
2024-08-28 18:20:28 +0530

    Move addToCart into cart.js

--- JavaScript-Amazon/data/cart.js
+++ JavaScript-Amazon/data/cart.js
@@ -1,1 +1,32 @@
 export const cart = [];
+
+export function addToCart(productId, quantitySelector, quantitySelectorValue){
+  let matchingItem;
+
+  cart.forEach((item) => {
+      if(productId === item.productId){
+      matchingItem = item;
+      }
+  });
+
+  if(matchingItem){
+      if(quantitySelector.value==1)
+      matchingItem.quantity += 1;
+      else
+      matchingItem.quantity += quantitySelectorValue;
+  }
+  else{
+      if(quantitySelector.value==1){
+      cart.push({
+          productId,
+          quantity: 1
+      });        
+      }
+      else{
+      cart.push({
+          productId,
+          quantity: quantitySelectorValue
+      });
+      }
+  }
+}
