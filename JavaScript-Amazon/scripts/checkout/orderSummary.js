115e4627106f4568610373a7dea70c362d8cf454
Ayush Tripathi <ayushtripathi1025@gmail.com>
2024-12-02 00:07:48 +0530

    Updated Checkout header using MVC Model & more

--- JavaScript-Amazon/scripts/checkout/orderSummary.js
+++ JavaScript-Amazon/scripts/checkout/orderSummary.js
@@ -2,8 +2,9 @@ import { cart , removeFromCart, calculateCartQuantity, updateQuantity, updateDel
 import { products , getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions , getDeliveryoption} from '../../data/deliveryOptions.js'; 
+import {deliveryOptions , getDeliveryoption, calculateDeliveryDate} from '../../data/deliveryOptions.js'; 
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML = '';
@@ -15,14 +16,7 @@ export function renderOrderSummary(){
     const deliveryOptionId = cartItem.deliveryOptionId;
     const deliveryOption = getDeliveryoption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString = deliveryDate.format(
-      'dddd, MMMM D'
-    );
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -72,14 +66,7 @@ export function renderOrderSummary(){
   function deliveryOptionsHTML(matchingProduct, cartItem) {
     let html = '';
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      const dateString = calculateDeliveryDate(deliveryOption);
 
       const priceString = deliveryOption.priceCents === 0? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} `;
 
@@ -121,11 +108,9 @@ export function renderOrderSummary(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
         updateCartQuantity();
+        renderCheckoutHeader();
+        renderOrderSummary();
         renderPaymentSummary();
       })
     });
